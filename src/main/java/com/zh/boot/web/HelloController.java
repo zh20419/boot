5d03c1325ddfe75354cf7f8f453a243b7d56f3a0
zhangheng <693671035@qq.com>
2018-10-14 17:43:29 +0800

    second commit

---
+++ src/main/java/com/zh/boot/web/HelloController.java
@@ -0,0 +1,14 @@
+package com.zh.boot.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "こにちは";
+    }
+
+}
